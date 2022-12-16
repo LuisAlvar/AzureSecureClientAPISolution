f480af7b47a5c921282e856eb484d6a9bbd5f492
luis <alvarez.en.luis@gmail.com>
2022-12-15 23:37:16 -0800

    rewrote the dev user secrets ps1 script

--- SecureAPI/Environments/Prod/LoadEnvrionmentVariables.ps1
+++ SecureAPI/Environments/Prod/LoadEnvrionmentVariables.ps1
@@ -8,20 +8,6 @@ Function LoadEnvironmentVariables
   
   Write-Host "###################### Load Environment Variables Production #######################"
 
-  $AzureKeyVault = [System.Environment]::GetEnvironmentVariable('Azure_Key_Vault_Name','Machine')
-
-  dotnet user-secrets clear --project ../../SecureAPI.csproj
-
-  #Azure_Key_Vault_Name
-  $Azure_Key_Vault_Name = [System.Environment]::GetEnvironmentVariable('Azure_Key_Vault_Name','Machine')
-  $UserSecret = $ClassName + ":" + "Azure_Key_Vault_Name"
-  Write-Host "--- Loading " $UserSecret
-  dotnet user-secrets set $UserSecret $Azure_Key_Vault_Name --project ../../SecureAPI.csproj
-
-  Write-Host "--- View the list of secrets"
-  dotnet user-secrets list --project ../../SecureAPI.csproj
-
-
 }
 
 LoadEnvironmentVariables
